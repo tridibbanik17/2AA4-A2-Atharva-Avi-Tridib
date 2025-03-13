845d1e0d6a0963601e55940992ac5437c11b4836
tridibbanik17 <tridib.perfect@gmail.com>
2025-03-13 19:51:29 -0400

    Added a Stage interface to represent a stage in drone's mission

---
+++ src/main/java/ca/mcmaster/se2aa4/island/team033/stage/Stage.java
@@ -0,0 +1,16 @@
+package ca.mcmaster.se2aa4.island.team033.stage;
+
+import org.json.JSONObject;
+
+import ca.mcmaster.se2aa4.island.team033.drone.Controller;
+import ca.mcmaster.se2aa4.island.team033.position.Direction;
+
+// Represent a stage in the drone's mission
+public interface Stage {
+
+    String getDroneCommand(Controller controller, Direction direction); // Get the drone's command based on the controller and the direction
+    void processInfo(JSONObject info); // Process information in JSON format
+    Stage getNextStage(); // Get the next stage in the process
+    boolean isFinished(); // Check if the current stage is finished
+    boolean isLastStage(); // Check if the current stage is the last one
+}
