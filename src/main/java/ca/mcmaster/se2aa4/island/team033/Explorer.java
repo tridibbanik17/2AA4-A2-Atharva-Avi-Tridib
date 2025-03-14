01e8245ad4e87a237f69830e9dec0b1f6e42e285
tridibbanik17 <tridib.perfect@gmail.com>
2025-03-14 00:36:47 -0400

    Edited Explorer class (close #4)

--- src/main/java/ca/mcmaster/se2aa4/island/team033/Explorer.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team033/Explorer.java
@@ -1,15 +1,29 @@
 package ca.mcmaster.se2aa4.island.team033;
 
-import eu.ace_design.island.bot.IExplorerRaid;
 import java.io.StringReader;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 
+import ca.mcmaster.se2aa4.island.team033.drone.Drone;
+import ca.mcmaster.se2aa4.island.team033.drone.SimpleDrone;
+import ca.mcmaster.se2aa4.island.team033.map.ListMap;
+import ca.mcmaster.se2aa4.island.team033.map.Map;
+import ca.mcmaster.se2aa4.island.team033.position.Direction;
+import ca.mcmaster.se2aa4.island.team033.report.NavigationReport;
+import ca.mcmaster.se2aa4.island.team033.report.Report;
+import ca.mcmaster.se2aa4.island.team033.search.GridSearch;
+import ca.mcmaster.se2aa4.island.team033.search.Search;
+import eu.ace_design.island.bot.IExplorerRaid;
+
 public class Explorer implements IExplorerRaid {
 
     private final Logger logger = LogManager.getLogger();
+    private Search gridSearch;
+    private Map map;
+    private Report report;
 
     @Override
     public void initialize(String s) {
@@ -18,33 +32,34 @@ public class Explorer implements IExplorerRaid {
         logger.info("** Initialization info:\n {}",info.toString(2));
         String direction = info.getString("heading");
         Integer batteryLevel = info.getInt("budget");
+
+        this.map = new ListMap();
+        Drone drone = new SimpleDrone(batteryLevel, Direction.fromSymbol(direction));
+        this.gridSearch = new GridSearch(drone);
+        this.report = new NavigationReport();
+
         logger.info("The drone is facing {}", direction);
         logger.info("Battery level is {}", batteryLevel);
     }
 
     @Override
     public String takeDecision() {
-        JSONObject decision = new JSONObject();
-        decision.put("action", "stop"); // we stop the exploration immediately
-        logger.info("** Decision: {}",decision.toString());
-        return decision.toString();
+        String command = gridSearch.performSearch();
+        logger.info("Command: {}", command);
+        return command;
     }
 
     @Override
     public void acknowledgeResults(String s) {
+        logger.info("Response: {}", s);
         JSONObject response = new JSONObject(new JSONTokener(new StringReader(s)));
-        logger.info("** Response received:\n"+response.toString(2));
-        Integer cost = response.getInt("cost");
-        logger.info("The cost of the action was {}", cost);
-        String status = response.getString("status");
-        logger.info("The status of the drone is {}", status);
-        JSONObject extraInfo = response.getJSONObject("extras");
-        logger.info("Additional information received: {}", extraInfo);
+        gridSearch.readResponse(response, map);
     }
 
     @Override
     public String deliverFinalReport() {
-        return "no creek found";
+        String finalReport = report.generateReport(map);
+        logger.info(finalReport);
+        return finalReport;
     }
-
 }
