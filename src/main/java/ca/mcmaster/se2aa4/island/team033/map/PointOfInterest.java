8759a036f224b0747185fab696d70e493e050d2b
tridibbanik17 <tridib.perfect@gmail.com>
2025-03-15 15:50:12 -0400

    Refactored map package

--- src/main/java/ca/mcmaster/se2aa4/island/team033/map/PointOfInterest.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team033/map/PointOfInterest.java
@@ -4,9 +4,9 @@ import ca.mcmaster.se2aa4.island.team033.position.Coordinate;
 
 public class PointOfInterest {
 
-    private final String id;     // unique identifier for poi
-    private final PointOfInterestType type; // type of poi
-    private final Coordinate location; // location of poi
+    private String id;     // unique identifier for poi
+    private PointOfInterestType type; // type of poi
+    private Coordinate location; // location of poi
 
     public PointOfInterest(String id, PointOfInterestType type, Coordinate location) {
         this.id = id;
