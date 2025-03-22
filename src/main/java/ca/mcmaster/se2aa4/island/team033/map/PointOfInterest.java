147c63ce0b043fe0fab8647027c306effffcc882
tridibbanik17 <tridib.perfect@gmail.com>
2025-03-22 00:23:59 -0400

    Updated version

--- src/main/java/ca/mcmaster/se2aa4/island/team033/map/PointOfInterest.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team033/map/PointOfInterest.java
@@ -4,9 +4,9 @@ import ca.mcmaster.se2aa4.island.team033.position.Coordinate;
 
 public class PointOfInterest {
 
-    private final String id;
-    private final PointOfInterestType type;
-    private final Coordinate location;
+    private final String id;     // unique identifier for poi
+    private final PointOfInterestType type; // type of poi
+    private final Coordinate location; // location of poi
 
     public PointOfInterest(String id, PointOfInterestType type, Coordinate location) {
         this.id = id;
