ab48968ff533c5196ec15bdd8d8899edbc1c7bad
tridibbanik17 <tridib.perfect@gmail.com>
2025-03-22 00:21:34 -0400

    Change return types for two methods

--- src/main/java/ca/mcmaster/se2aa4/island/team033/drone/Drone.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team033/drone/Drone.java
@@ -6,8 +6,8 @@ import ca.mcmaster.se2aa4.island.team033.position.Direction;
 // Define the operations for the drone state and the actions.
 public interface Drone {
 
-    Integer getBatteryLevel(); // get current battery level
-    void drainBattery(Integer cost); // drain battery by a specific cost
+    int getBatteryLevel(); // get current battery level
+    void drainBattery(int cost); // drain battery by a specific cost
     Direction getHeading(); // get current heading direction
     Coordinate getLocation(); // Get current location
     void moveForward(); // Move forward by one unit in current direction
