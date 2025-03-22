f4029850d69f3c4a75e0fffe625a18e6d94584a7
tridibbanik17 <tridib.perfect@gmail.com>
2025-03-22 16:20:04 -0400

    Tested one more point of interest type

--- src/test/java/ca/mcmaster/se2aa4/island/team033/PointOfInterestTest.java
+++ src/test/java/ca/mcmaster/se2aa4/island/team033/PointOfInterestTest.java
@@ -8,22 +8,28 @@ import ca.mcmaster.se2aa4.island.team033.map.PointOfInterestType;
 import ca.mcmaster.se2aa4.island.team033.position.Coordinate;
 
 public class PointOfInterestTest {
-    private final PointOfInterest poi = new PointOfInterest("1234567890", PointOfInterestType.CREEK, new Coordinate(7, 19));
+    private final PointOfInterest poi1 = new PointOfInterest("01829792322", PointOfInterestType.EMERGENCY_SITE, new Coordinate(13, -16));
+    private final PointOfInterest poi2 = new PointOfInterest("2#01715170", PointOfInterestType.CREEK, new Coordinate(19, -23));
+
+    @Test
+    public void testGetID() {
+        assertEquals("01829792322", poi1.getId());
+        assertEquals("2#01715170", poi2.getId());
+    }
 
     @Test
     public void testGetKind() {
-        assertEquals(PointOfInterestType.CREEK, poi.getType());
+        assertEquals(PointOfInterestType.EMERGENCY_SITE, poi1.getType());
+        assertEquals(PointOfInterestType.CREEK, poi2.getType());
     }
 
     @Test
     public void testGetLocation() {
-        Coordinate coord = new Coordinate(7, 19);
-        assertEquals(coord.getX(), poi.getLocation().getX());
-        assertEquals(coord.getY(), poi.getLocation().getY());
-    }
-
-    @Test
-    public void testGetID() {
-        assertEquals("1234567890", poi.getId());
+        Coordinate coord1 = new Coordinate(13, -16);
+        Coordinate coord2 = new Coordinate(19, -23);
+        assertEquals(coord1.getX(), poi1.getLocation().getX());
+        assertEquals(coord1.getY(), poi1.getLocation().getY());
+        assertEquals(coord2.getX(), poi2.getLocation().getX());
+        assertEquals(coord2.getY(), poi2.getLocation().getY());
     }
 }
