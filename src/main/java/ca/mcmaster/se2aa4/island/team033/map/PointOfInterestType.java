9ce48cb80a3aa529ab0f755390a4fd47626215d5
tridibbanik17 <tridib.perfect@gmail.com>
2025-03-23 15:09:10 -0400

    Add Comments (documentation) following OOP Self-documenting priniciple for future work

--- src/main/java/ca/mcmaster/se2aa4/island/team033/map/PointOfInterestType.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team033/map/PointOfInterestType.java
@@ -1,5 +1,7 @@
-package ca.mcmaster.se2aa4.island.team033.map;    
-    
+package ca.mcmaster.se2aa4.island.team033.map;
+
+// Enum representing the types of points of interest.
+// This enum ensures type safety and restricts the possible types to CREEK and EMERGENCY_SITE.
 public enum PointOfInterestType {
     CREEK, 
     EMERGENCY_SITE
