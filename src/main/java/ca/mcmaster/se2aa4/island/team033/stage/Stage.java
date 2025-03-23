f20b2d176ba8fc9a4b69fad5f740073d36c466e9
tridibbanik17 <tridib.perfect@gmail.com>
2025-03-23 15:27:29 -0400

    Add Comments (documentation) following OOP Self-documenting priniciple for future work

--- src/main/java/ca/mcmaster/se2aa4/island/team033/stage/Stage.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team033/stage/Stage.java
@@ -5,13 +5,11 @@ import org.json.JSONObject;
 import ca.mcmaster.se2aa4.island.team033.drone.Controller;
 import ca.mcmaster.se2aa4.island.team033.position.Direction;
 
-/**
- * Interface representing a stage in the drone's mission.
- * Each stage defines the behavior of the drone during that stage.
- */
+// Interface representing a stage in the drone's mission.
+// Each stage defines the behaviour of the drone during that stage.
 public interface Stage {
-    String getDroneCommand(Controller controller, Direction direction); // Get the drone's command based on the controller and direction
-    void processInfo(JSONObject info); // Process information received from the drone's sensors
+    String getDroneCommand(Controller controller, Direction direction); // Returns the drone command based on current state and direction.
+    void processInfo(JSONObject info); // Process information received from the drone
     Stage getNextStage(); // Determine the next stage in the mission
     boolean isFinished(); // Check if the current stage is completed
     boolean isLastStage(); // Check if the current stage is the final one in the mission
