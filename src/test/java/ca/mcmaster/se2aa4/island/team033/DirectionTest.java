b98c1a36e47cbc3dedc39c2b8313554e794c74e4
tridibbanik17 <tridib.perfect@gmail.com>
2025-03-23 18:02:31 -0400

    Tested different directions

--- src/test/java/ca/mcmaster/se2aa4/island/team033/DirectionTest.java
+++ src/test/java/ca/mcmaster/se2aa4/island/team033/DirectionTest.java
@@ -6,25 +6,25 @@ import org.junit.jupiter.api.Test;
 import ca.mcmaster.se2aa4.island.team033.position.Direction;
 
 public class DirectionTest {
-    private final Direction dir1 = Direction.EAST;
-    private final Direction dir2 = Direction.SOUTH;
+    private final Direction dirEast = Direction.EAST;
+    private final Direction dirSouth = Direction.SOUTH;
 
     @Test
     public void testGetRight() {
-        assertEquals(Direction.SOUTH, dir1.getRight());
-        assertEquals(Direction.WEST, dir2.getRight());
+        assertEquals(Direction.SOUTH, dirEast.getRight());
+        assertEquals(Direction.WEST, dirSouth.getRight());
     }
 
     @Test
     public void testGetLeft() {
-        assertEquals(Direction.NORTH, dir1.getLeft());
-        assertEquals(Direction.EAST, dir2.getLeft());
+        assertEquals(Direction.NORTH, dirEast.getLeft());
+        assertEquals(Direction.EAST, dirSouth.getLeft());
     }
 
     @Test
     public void testGetSymbol() {
-        assertEquals("E", dir1.getSymbol());
-        assertEquals("S", dir2.getSymbol());
+        assertEquals("E", dirEast.getSymbol());
+        assertEquals("S", dirSouth.getSymbol());
     }
 
     @Test
