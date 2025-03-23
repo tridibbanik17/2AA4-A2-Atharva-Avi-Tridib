73101be2299b835b5405c605a2fa9b778a3405b3
tridibbanik17 <tridib.perfect@gmail.com>
2025-03-23 15:08:53 -0400

    Add Comments (documentation) following OOP Self-documenting priniciple for future work

--- src/main/java/ca/mcmaster/se2aa4/island/team033/map/Map.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team033/map/Map.java
@@ -1,7 +1,9 @@
 package ca.mcmaster.se2aa4.island.team033.map;
 
+// Defines the contract for managing points of interest on a map.
+// This interface abstracts the implementation details of how points of interest are stored and retrieved.
 public interface Map {
-    void addPointOfInterest(PointOfInterest poi);
-    String getEmergencySiteID();
-    String getClosestCreekID();
+    void addPointOfInterest(PointOfInterest poi); // Adds a point of interest to the map.
+    String getEmergencySiteID(); // Retrieves the ID of the emergency site.
+    String getClosestCreekID(); // Retrieves the ID of the closest creek to the emergency site.
 }
