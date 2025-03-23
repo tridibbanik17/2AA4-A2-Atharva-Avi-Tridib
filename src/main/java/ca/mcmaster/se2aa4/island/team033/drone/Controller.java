d7c2788f538b5981459ea6ee62a3eb1401acd4b1
tridibbanik17 <tridib.perfect@gmail.com>
2025-03-23 15:13:06 -0400

    Add Comments (documentation) following OOP Self-documenting priniciple for future work

--- src/main/java/ca/mcmaster/se2aa4/island/team033/drone/Controller.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team033/drone/Controller.java
@@ -2,12 +2,11 @@ package ca.mcmaster.se2aa4.island.team033.drone;
 
 import ca.mcmaster.se2aa4.island.team033.position.Direction;
 
-// Define the behaviours for interacting with the drone.
+// Defines the command interface for interacting with the drone.
 public interface Controller {
-
-    String flyCommand(); // Command to fly forward
-    String headingCommand(Direction dir); // Command to change the heading direction
-    String echoCommand(Direction dir); // Command to echo the environment in a specific direction
-    String scanCommand(); // Command to scan the surroundings
-    String stopCommand(); // Command to stop
+    String flyCommand(); // Executes fly command and returns response.
+    String headingCommand(Direction dir); // Executes heading change command and returns response.
+    String echoCommand(Direction dir); // Executes echo command and returns response.
+    String scanCommand(); // Executes scan command and returns response.
+    String stopCommand(); // Executes stop command and returns response.
 }
