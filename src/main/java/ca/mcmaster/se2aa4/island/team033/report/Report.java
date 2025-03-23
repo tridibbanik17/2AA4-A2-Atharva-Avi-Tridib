c369ac29638c850954f3a23a15b8f5ce11349e9c
tridibbanik17 <tridib.perfect@gmail.com>
2025-03-23 15:09:32 -0400

    Add Comments (documentation) following OOP Self-documenting priniciple for future work

--- src/main/java/ca/mcmaster/se2aa4/island/team033/report/Report.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team033/report/Report.java
@@ -2,7 +2,9 @@ package ca.mcmaster.se2aa4.island.team033.report;
 
 import ca.mcmaster.se2aa4.island.team033.map.Map;
 
+// Defines the contract for generating reports based on a Map.
+// This interface abstracts the implementation details of report generation.
 public interface Report {
-    // Report is generated as a String for a Map.
+    // Generates a report as a String for the given Map.
     String generateReport(Map map);
 }
